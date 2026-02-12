92572ff2913b4fe1580fd22549e3a100a1670f6c
Yusuf-Abidin-Nurrahman <yusufabidin02@gmail.com>
2026-02-12 13:42:12 +0700

    validasi store Branch ke StoreBranchRequest

--- app/Http/Controllers/BranchController.php
+++ app/Http/Controllers/BranchController.php
@@ -2,55 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreBranchRequest;
 use App\Models\Branch;
 use Illuminate\Http\Request;
 
 class BranchController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreBranchRequest $request)
     {
-        $validated = $request->validate([
-            'name' => 'required|string|max:255',
-            'area_id' => 'required|exists:areas,id'
-        ]);
+        $validated = $request->validated();
 
         Branch::create($validated);
         return back()->with('success', 'Cabang berhasil dibuat.');
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(Branch $branch)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Branch $branch)
-    {
-        //
     }
 
     /**
